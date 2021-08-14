0c773dab2e9c82e3b5ecd390cacf6adc9447a3ca
xer <xer@xer-labtop>
2021-08-14 14:24:24 +0300

    implemented back view and bottom view

--- terminal_only.py
+++ terminal_only.py
@@ -33,7 +33,7 @@ def view_top(data3d, height, width):
 
 def view_bottom(data3d, height, width):
     view = [[' ' for x in range(width)] for y in range(height)]
-    for y in range(len(data3d[0])):
+    for y in range(len(data3d[0]) - 1, 0, -1):
         for z in range(len(data3d)):
             for x in range(len(data3d[0][0])):
                 if view[z][x] == ' ':
@@ -43,6 +43,15 @@ def view_bottom(data3d, height, width):
 def view_front(data3d, height, width):
     view = [[' ' for x in range(width)] for y in range(height)]
     for z in range(len(data3d)):
+        for y in range(len(data3d[0])):
+            for x in range(len(data3d[0][0])):
+                if view[y][x] == ' ':
+                    view[y][x] = data3d[z][y][x]
+    return view
+
+def view_back(data3d, height, width):
+    view = [[' ' for x in range(width)] for y in range(height)]
+    for z in range(len(data3d) - 1, 0, -1):
         for y in range(len(data3d[0])):
             for x in range(len(data3d[0][0])):
                 if view[y][x] == ' ':
@@ -72,12 +81,16 @@ views['0 0'] = 'top'
 views['0 1'] = 'front'
 views['1 0'] = 'left'
 views['1 1'] = 'right'
+views['0 2'] = 'bottom'
+views['1 2'] = 'back'
 
 functions = {}
 functions['0 0'] = view_top
 functions['0 1'] = view_front
 functions['1 0'] = view_left
 functions['1 1'] = view_right
+functions['0 2'] = view_bottom
+functions['1 2'] = view_back
 
 
 def move3d(cursor3d, y, x, i, j):
@@ -86,7 +99,13 @@ def move3d(cursor3d, y, x, i, j):
     if view_name == 'top':
         cursor3d[0] += y
         cursor3d[2] += x
+    elif view_name == 'bottom':
+        cursor3d[0] += y
+        cursor3d[2] += x
     elif view_name == 'front':
+        cursor3d[1] += y
+        cursor3d[2] += x
+    elif view_name == 'back':
         cursor3d[1] += y
         cursor3d[2] += x
     elif view_name == 'left':
@@ -106,7 +125,13 @@ def cursor2d(cursor3d, i, j):
     if view_name == 'top':
         y = cursor3d[0]
         x = cursor3d[2]
+    elif view_name == 'bottom':
+        y = cursor3d[0]
+        x = cursor3d[2]
     elif view_name == 'front':
+        y = cursor3d[1]
+        x = cursor3d[2]
+    elif view_name == 'back':
         y = cursor3d[1]
         x = cursor3d[2]
     elif view_name == 'left':
@@ -140,7 +165,7 @@ curses.noecho()
 curses.cbreak()
 stdscr.keypad(True)
 begin_x = 0; begin_y = 0
-wins = [[curses.newwin(height, width, begin_y, begin_x) for begin_x in [0,width]] for begin_y in [0, height]]
+wins = [[curses.newwin(height, width, begin_y, begin_x) for begin_x in [0,width,width * 2]] for begin_y in [0, height]]
 for win_rows in wins:
     for win in win_rows:
         win.keypad(True)
@@ -180,16 +205,12 @@ while ch != 'q':
     x = 0
     y = 0
     if ch == 'KEY_UP':
-        ys[i][j] -= 1
         y = -1
     elif ch == 'KEY_DOWN':
-        ys[i][j] += 1
         y = 1
     elif ch == 'KEY_LEFT':
-        xs[i][j] -= 1
         x = -1
     elif ch == 'KEY_RIGHT':
-        xs[i][j] += 1
         x = 1
     elif ch == 'k':
         i -= 1
@@ -202,7 +223,7 @@ while ch != 'q':
     elif len(ch) == 1:
         alldata3d['{} {} {}'.format(*cursor3d)] = ch
         #data3d[cursor3d[0]][cursor3d[1]][cursor3d[2]] = ch
-    i, j = check_limits(i, j, 2, 2)
+    i, j = check_limits(i, j, 2, 3)
     cursor3d = move3d(cursor3d, y, x, i, j)
     data3d = all_to_data3d(data3d_blank, alldata3d, cursor3d)
     for I in range(len(wins)):
